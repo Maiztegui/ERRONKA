ebbb88b2bc537b84e5b557c013f7c93affd86ed8
Maiztegui <webnmaiztegi@imaltuna.com>
2023-11-02 12:07:24 +0100

    Kalkulagaiua added  to dev

--- Kalkulagaiua/src/Main.java
+++ Kalkulagaiua/src/Main.java
@@ -1,17 +1,26 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+    public static void main(String[] args){
+        batuketa();
     }
+    public static void batuketa() {
+        Scanner num=new Scanner(System.in);
+
+        Double zenbakia1, zenbakia2,emaitza;
+
+        System.out.println("===================================");
+        System.out.println("====         BATUKETA           ====");
+        System.out.println("===================================");
+        System.out.println("Sartu lehenengo zenbakia");
+        zenbakia1=num.nextDouble();
+        System.out.println("Sartu bigarren zenbakia");
+        zenbakia2=num.nextDouble();
+
+        emaitza=zenbakia1+zenbakia2;
+        System.out.println("Sartutako zenbakien batuketa "+emaitza+" da!");
+    }
+
 }
