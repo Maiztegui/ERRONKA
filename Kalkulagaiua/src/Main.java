225db707d43d32779b75fff71f7fb559ed8d50a6
Maiztegui <webnmaiztegi@imaltuna.com>
2023-11-17 10:13:10 +0100

    cambio de enunciado final

--- Kalkulagaiua/src/Main.java
+++ Kalkulagaiua/src/Main.java
@@ -1,17 +1,79 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Kalkulagaiua {
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        int zenbakia;
+        menua();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Aukeratu eragiketa bat; idatzi dagokion zenbakia");
+        zenbakia = sc.nextInt();
+        while (zenbakia!=7){
+            switch (zenbakia){
+                case 1:
+                    batuketa();
+                    break;
+                case 2:
+                    kenketa();
+                    break;
+                case 3:
+                    biderketa();
+                    break;
+                case 4:
+                    zatiketa();
+                    break;
+                case 5:
+                    berreketa();
+                    break;
+                case 6:
+                    erroketa();
+                    break;
+                default:
+                    System.out.println("Sartutako zenbakia ez dago aukeren artean");
+            }
+            menua();
+            zenbakia = sc.nextInt();
         }
     }
+
+    private static void menua() {
+        System.out.println("===================================================================================");
+        System.out.println("==================             MALTUNA KALKULAGAILUA              =================");
+        System.out.println("===================================================================================");
+        System.out.println("===== Ondorengo eragiketak egin ditzaket, sakatu zenbakia eta lagunduko dizut! ====");
+        System.out.println("== 1. BATUKETA                                                                   ==");
+        System.out.println("== 2. KENKETA                                                                    ==");
+        System.out.println("== 3. BIDERKETA                                                                  ==");
+        System.out.println("== 4. ZATIKETA                                                                   ==");
+        System.out.println("== 5. BERREKETA                                                                  ==");
+        System.out.println("== 6. ERROKETA                                                                   ==");
+        System.out.println("== 7. BUKATU PROGRAMA                                                            ==");
+        System.out.println("===================================================================================");
+    }
+
+
+
+    public static void batuketa() {
+
+
+    }
+
+    public static void biderketa() {
+
+    }
+
+    public static void erroketa() {
+
+    }
+    public static void kenketa() {
+
+    }
+
+    public static void berreketa() {
+
+    }
+    public static void zatiketa() {
+
+    }
+
 }
+
